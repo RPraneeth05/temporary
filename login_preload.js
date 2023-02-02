36303b4b6e3ff224d8b19c503762bd9f303e4784
Srithwak <60199432+Srithwak@users.noreply.github.com>
2023-02-01 20:49:11 -0500

    Update login_preload.js

--- login_preload.js
+++ login_preload.js
@@ -1,26 +1,58 @@
+// const path = require('path');
+// const fs = require('fs');
+
+// function readFromJSON(file) {
+//    return JSON.parse(fs.readFileSync(file));
+// }
+
+// function writeToJSON(file, data) {
+//    fs.writeFileSync(file, JSON.stringify(data, null, 2), {
+//       encoding: 'utf-8',
+//       flag: 'w'
+//    });
+//    console.log(`Wrote ${data} to ${file}`);
+// }
+
+// function validateUsernameAndPassword() {
+//    let users = readFromJSON(path.join(__dirname, './src/database/accounts.json'));
+//    let usernameInput = document.querySelector('.username').value;
+//    let passwordInput = document.querySelector('.password').value;
+//    for (user of users) {
+//       if (usernameInput === user.username && passwordInput === user.password) {
+//          if (user.admin) location.href = path.join(__dirname, './src/main/admin.html');
+//          else location.href = path.join(__dirname, './src/main/student.html');
+//       }
+//    }
+// }
+
 const path = require('path');
 const fs = require('fs');
 
 function readFromJSON(file) {
-   return JSON.parse(fs.readFileSync(file));
+    return JSON.parse(fs.readFileSync(file));
 }
 
 function writeToJSON(file, data) {
-   fs.writeFileSync(file, JSON.stringify(data, null, 2), {
-      encoding: 'utf-8',
-      flag: 'w'
-   });
-   console.log(`Wrote ${data} to ${file}`);
+    fs.writeFileSync(file, JSON.stringify(data, null, 2), {
+        encoding: 'utf-8',
+        flag: 'w'
+    });
+    console.log(`Wrote ${data} to ${file}`);
 }
 
 function validateUsernameAndPassword() {
-   let users = readFromJSON(path.join(__dirname, './src/database/accounts.json'));
-   let usernameInput = document.querySelector('.username').value;
-   let passwordInput = document.querySelector('.password').value;
-   for (user of users) {
-      if (usernameInput === user.username && passwordInput === user.password) {
-         if (user.admin) location.href = path.join(__dirname, './src/main/admin.html');
-         else location.href = path.join(__dirname, './src/main/student.html');
-      }
-   }
+    let users = readFromJSON(path.join(__dirname, './src/database/accounts.json'));
+    let usernameInput = document.querySelector('.username').value;
+    let passwordInput = document.querySelector('.password').value;
+    for (user of users) {
+        if (usernameInput === user.username && passwordInput === user.password) {
+            localStorage.setItem("key", JSON.stringify(user).fname);
+            if (user.admin) 
+                location.href = path.join(__dirname, './src/main/admin.html');
+            
+            else 
+                location.href = path.join(__dirname, './src/main/student.html');
+            
+        }
+    }
 }
